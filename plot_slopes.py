f62f64474ed58893013227dbc128aa80faa27f39
wjr21 <wjr21@sussex.ac.uk>
2024-06-27 15:54:16 +0100

    Skip empty snaps

--- plot_slopes.py
+++ plot_slopes.py
@@ -81,6 +81,8 @@ extent = (-2.7, 0, -2.7, 0)
 
 # Loop over the snapshots
 for snap in snaps:
+    if len(att_slopes["attenuated"][snap]) == 0:
+        continue
     # Plot hexbins of slope vs slope for all galaxies and the red sample
     fig, axs = plt.subplots(1, 2, figsize=(7, 3.5))
 
