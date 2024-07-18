4643b7565f5968a354a87be4136b6117211d5d79
wjr21 <wjr21@sussex.ac.uk>
2024-07-18 16:39:26 +0100

    PLotting gas and dust size

---
+++ plot_gas_size.py
@@ -0,0 +1,119 @@
+"""A script for comparing the gas distributions of galaxies."""
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+from synthesizer.grid import Grid
+
+from utils import get_masked_synth_data, get_synth_data, savefig, SNAPSHOTS
+
+# Define the parser
+parser = argparse.ArgumentParser(description="Plot the SFZHs of galaxies.")
+parser.add_argument(
+    "--type",
+    type=str,
+    default="stellar",
+    help="The type of data to plot.",
+)
+parser.add_argument(
+    "--grid",
+    type=str,
+    help="The file name of the Synthesizer gird.",
+)
+parser.add_argument(
+    "--grid_dir",
+    type=str,
+    help="The directory of the Synthesizer grid.",
+)
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Define the data file
+if args.type == "stellar":
+    data_file = "data/pure_stellar_<region>_<snap>.hdf5"
+elif args.type == "agn":
+    data_file = "data/pure_agn_<region>_<snap>.hdf5"
+else:
+    data_file = "data/combined_<region>_<snap>.hdf5"
+
+# Load the grid
+grid = Grid(args.grid, args.grid_dir)
+
+# Define plotting parameters
+gridsize = 30
+extent = (0, 10, 0, 10)
+norm = mcolors.LogNorm(1, 10**3.5, clip=True)
+
+
+# Get the synthesizer data
+(
+    fluxes,
+    colors,
+    red1,
+    red2,
+    sizes,
+    masks,
+    indices,
+    weights,
+) = get_synth_data(data_file, "attenuated", get_weights=True)
+
+# Get the sfzhs
+gas_hmr = get_masked_synth_data(data_file, "GasHalfMassRadius")
+dust_hmr = get_masked_synth_data(data_file, "DustHalfMassRadius")
+
+# Loop over the snapshots
+for snap in SNAPSHOTS:
+    # Get the data
+    gas_data = gas_hmr[snap]
+    dust_data = dust_hmr[snap]
+    ws = weights[snap]
+    mask = masks[snap]
+
+    # Create the figure
+    fig = plt.figure(figsize=(3.5, 3.5))
+    ax = fig.add_subplot(111)
+
+    # Draw a grid behind everything
+    ax.grid(True)
+    ax.set_axisbelow(True)
+
+    # Plot the non-LRD data as a hexbin
+    hb = ax.hexbin(
+        gas_data[~mask],
+        dust_data[~mask],
+        gridsize=gridsize,
+        norm=norm,
+        extent=extent,
+        cmap="viridis",
+        linewidths=0.2,
+        xscale="log",
+        yscale="log",
+        mincnt=1,
+        C=ws[~mask],
+    )
+
+    # Plot the LRD data as a scatter plot
+    ax.scatter(
+        gas_data[mask],
+        dust_data[mask],
+        c="red",
+        s=1,
+        alpha=0.8,
+        label="LRD",
+    )
+
+    # Add the colorbar
+    cbar = fig.colorbar(hb, ax=ax)
+    cbar.set_label(r"$\sum w_i$")
+
+    # Set the labels
+    ax.set_xlabel(r"$R_{1/2}^{\mathrm{Gas}} /$ [kpc]")
+    ax.set_ylabel(r"$R_{1/2}^{\mathrm{Dust}} /$ [kpc]")
+
+    # Draw the legend
+    ax.legend()
+
+    # Save the figure
+    savefig(fig, f"gas_dust_size_{args.type}_{snap}")
