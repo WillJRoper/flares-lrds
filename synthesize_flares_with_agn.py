4e5334479322f63e0eef5ce31669166b4b9c3800
wjr21 <wjr21@sussex.ac.uk>
2024-08-19 18:05:43 +0100

    Fixing out path

--- synthesize_flares_with_agn.py
+++ synthesize_flares_with_agn.py
@@ -878,7 +878,7 @@ if __name__ == "__main__":
     nthreads = args.nthreads
 
     # Define the output path
-    outpath = f"data/pure_stellar_{str(region).zfill(2)}_{snap}.hdf5"
+    outpath = f"data/combined_{str(region).zfill(2)}_{snap}.hdf5"
 
     # If the output already exists just exit
     if os.path.exists(outpath):
