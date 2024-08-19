02f0d066af6f22d4e560678c47a031474040e353
wjr21 <wjr21@sussex.ac.uk>
2024-08-19 18:16:52 +0100

    Don't need save spectra now

--- synthesize_flares_with_agn.py
+++ synthesize_flares_with_agn.py
@@ -268,8 +268,6 @@ def get_emission_model(
         "old_reprocessed",
         "young_attenuated",
         "old_attenuated",
-        "AGN_intrinsic",
-        "AGN_attenuated",
     ),
 ):
     """Get a StellarEmissionModel."""
