Correcting units

plot_lrd_counts.py
@@ -51,7 +51,7 @@ master_file = (
 flux_bins = np.logspace(-3, 1, 30)
 
 # Define mass bins
-mass_bins = np.logspace(8, 12, 30)
+mass_bins = np.logspace(8 / 10, 12 / 10, 30)
 
 # Get the masses from the master file
 masses = get_master_data(master_file, indices, "Mstar_aperture/30")