620fcb188b12b7666a33c9f7769c51414289b6cc
wjr21 <wjr21@sussex.ac.uk>
2024-08-29 12:26:56 +0100

    Adjusting bins

--- plot_luminosity_function.py
+++ plot_luminosity_function.py
@@ -128,7 +128,7 @@ for snap in SNAPSHOTS:
 
 
 # Define magnitude bins
-bins = np.logspace(-2, np.log10(1500), 30)
+bins = np.logspace(0, np.log10(1500), 30)
 bin_cents = (bins[:-1] + bins[1:]) / 2
 
 # Define the volume
