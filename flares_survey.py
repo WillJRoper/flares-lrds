f063e09d662230df0be9ba5c4608b5dc0c62ecee
wjr21 <wjr21@sussex.ac.uk>
2024-10-25 13:12:52 +0100

    Typo

--- flares_survey.py
+++ flares_survey.py
@@ -381,7 +381,7 @@ if __name__ == "__main__":
     survey.get_photometry_luminosities()
     survey.get_photometry_fluxes()
     survey.get_images_luminosity(fov=61 * kpc, kernel=kernel_data)
-    survey.get_images_fluxes(fov=61 * kpc, kernel=kernel_data)
+    survey.get_images_flux(fov=61 * kpc, kernel=kernel_data)
 
     # Save the survey
     survey.write(outpath)
