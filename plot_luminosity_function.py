2fe5a4175b1be223525086ce333eec5f25f2ce79
wjr21 <wjr21@sussex.ac.uk>
2024-12-11 09:45:53 +0000

    New UVLF with UV1500 and new files

--- plot_luminosity_function.py
+++ plot_luminosity_function.py
@@ -1,13 +1,14 @@
 """A script for plotting the luminsoity function of different galaxy types."""
+
 import argparse
+
+import h5py
 import matplotlib.pyplot as plt
 import numpy as np
-from astropy.cosmology import Planck15 as cosmo
-
-from synthesizer.conversions import lnu_to_absolute_mag, fnu_to_lnu
-
-from utils import get_synth_data, SNAPSHOTS, savefig
 from synthesize_flares import get_flares_filters
+from synthesizer.conversions import lnu_to_absolute_mag
+
+from utils import SNAPSHOTS, savefig
 
 # Define the parser
 parser = argparse.ArgumentParser(
@@ -16,7 +17,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "--spec-type",
     type=str,
-    default="attenuated",
+    default="total",
     help="The type of data to plot.",
 )
 
@@ -24,22 +25,10 @@ parser.add_argument(
 args = parser.parse_args()
 
 # Define the data file
-data_file = "data/combined_<region>_<snap>.hdf5"
+data_file = "data/combined_<snap>.hdf5"
 
 # Define the filters we'll use
 filters = get_flares_filters("lrd_filters.hdf5")
-
-# Get the fluxes, colors and masks
-(
-    fluxes,
-    colors,
-    red1,
-    red2,
-    sizes,
-    masks,
-    _,
-    weights,
-) = get_synth_data(data_file, args.spec_type, get_weights=True)
 
 # Define magnitude bins
 bins = np.arange(-25, -15.5, 1)
@@ -50,28 +39,23 @@ volume = 3200**3  # Mpc^3
 
 # Loop over snapshots
 for snap in SNAPSHOTS:
+    print(f"Plotting {snap}")
+
     # Get the redshift from the snap tag
     z = float(snap.split("_")[-1].replace("z", "").replace("p", "."))
 
-    # Find the filter containing 1500 Angstrom
-    filt = filters.find_filter(
-        2000,
-        redshift=z,
-        method="transmission",
-    )
-
-    # Get the flux for this filter
-    flux = fluxes[snap][filt.filter_code.split(".")[-1]]
-
-    # Get the right mask
-    mask = masks[snap]
+    # Read the UV1500 luminosities, weights and LRD mask
+    with h5py.File(data_file, "r") as hdf:
+        lnu = hdf[f"Galaxies/Photometry/Luminosities/{args.spec_type}/UV1500"][...]
+        weights = hdf["Galaxies/RegionWeight"][...]
+        mask = hdf[f"Galaxieis/LRDFlag/{args.spec_type}"][...]
 
     # Convert flux to absolute magnitude
-    mags = lnu_to_absolute_mag(fnu_to_lnu(flux, cosmo, z))
+    mags = lnu_to_absolute_mag(lnu)
 
     # Compute the luminosity function full
     hist, _ = np.histogram(mags, bins=bins)
-    whist, _ = np.histogram(mags, bins=bins, weights=weights[snap])
+    whist, _ = np.histogram(mags, bins=bins, weights=weights)
     hist = hist.astype(float)
     whist = whist.astype(float)
     hist *= whist
@@ -79,7 +63,7 @@ for snap in SNAPSHOTS:
 
     # Compute the LRD luminosity function (masked LF)
     hist, _ = np.histogram(mags[mask], bins=bins)
-    whist, _ = np.histogram(mags[mask], bins=bins, weights=weights[snap][mask])
+    whist, _ = np.histogram(mags[mask], bins=bins, weights=weights[mask])
     hist = hist.astype(float)
     whist = whist.astype(float)
     hist *= whist
@@ -119,8 +103,7 @@ for snap in SNAPSHOTS:
 
     savefig(
         fig,
-        f"UVLF/luminosity_function_{args.spec_type}_"
-        f"{snap}_{filt.filter_code.replace('/', '')}",
+        f"UVLF/luminosity_function_{args.spec_type}_{snap}",
     )
 
 
@@ -136,33 +119,23 @@ for snap in SNAPSHOTS:
     # Get the redshift from the snap tag
     z = float(snap.split("_")[-1].replace("z", "").replace("p", "."))
 
-    # Find the filter containing 1500 Angstrom
-    filt = filters.find_filter(
-        2000,
-        redshift=z,
-        method="transmission",
-    )
-
-    # Get the flux for this filter
-    flux = fluxes[snap][filt.filter_code.split(".")[-1]]
-
-    # Get the right mask
-    mask = masks[snap]
-
-    # Convert flux to absolute magnitude
-    mags = fnu_to_lnu(flux, cosmo, z)
+    # Read the UV1500 luminosities, weights and LRD mask
+    with h5py.File(data_file, "r") as hdf:
+        lnu = hdf[f"Galaxies/Photometry/Luminosities/{args.spec_type}/UV1500"][...]
+        weights = hdf["Galaxies/RegionWeight"][...]
+        mask = hdf[f"Galaxieis/LRDFlag/{args.spec_type}"][...]
 
     # Compute the luminosity function full
-    hist, _ = np.histogram(mags, bins=bins)
-    whist, _ = np.histogram(mags, bins=bins, weights=weights[snap])
+    hist, _ = np.histogram(lnu, bins=bins)
+    whist, _ = np.histogram(lnu, bins=bins, weights=weights)
     hist = hist.astype(float)
     whist = whist.astype(float)
     hist *= whist
     phi = hist / volume / np.diff(bins)
 
     # Compute the LRD luminosity function (masked LF)
-    hist, _ = np.histogram(mags[mask], bins=bins)
-    whist, _ = np.histogram(mags[mask], bins=bins, weights=weights[snap][mask])
+    hist, _ = np.histogram(lnu[mask], bins=bins)
+    whist, _ = np.histogram(lnu[mask], bins=bins, weights=weights[mask])
     hist = hist.astype(float)
     whist = whist.astype(float)
     hist *= whist
@@ -200,6 +173,5 @@ for snap in SNAPSHOTS:
 
     savefig(
         fig,
-        f"UVLF/luminosity_function_nomag_{args.spec_type}_"
-        f"{snap}_{filt.filter_code.replace('/', '')}",
+        f"UVLF/luminosity_function_nomag_{args.spec_type}_{snap}",
     )
