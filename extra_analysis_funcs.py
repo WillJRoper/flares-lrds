30520694ca348ecf6313fe90130a11fa7c108277
wjr21 <wjr21@sussex.ac.uk>
2024-12-17 14:45:16 +0000

    Debugging

--- extra_analysis_funcs.py
+++ extra_analysis_funcs.py
@@ -374,6 +374,8 @@ def get_black_hole_data(gal):
         data["AverageAccretionRate"] = np.mean(gal.black_holes.accretion_rates)
         data["NumberOfBHs"] = gal.black_holes.nbh
 
+    print(data)
+
     return data
 
 
